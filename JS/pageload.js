830ab715518f7a15c95d28425944af0488b6fb6d
trogy <33707105+trogy@users.noreply.github.com>
2019-08-19 13:20:07 +1200

    Added Server Page Added AJAX JS for server page

--- JS/pageload.js
+++ JS/pageload.js
@@ -1,6 +1,11 @@
 //load page content
 $(document).ready(function(){
     $("#home").click(function(){
-        $("#page-content").load("home.html");
+        $("#page-content").load("Pages/home.html");
+    });
+});
+$(document).ready(function(){
+    $("#servers").click(function(){
+        $("#page-content").load("Pages/servers.html");
     });
 });
