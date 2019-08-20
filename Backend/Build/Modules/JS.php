69834318b22d986e1f88a48f47d43067a9f13f83
trogy <33707105+trogy@users.noreply.github.com>
2019-08-20 23:25:43 +1200

    Removed integrity check

--- Backend/Build/Modules/JS.php
+++ Backend/Build/Modules/JS.php
@@ -1,5 +1,5 @@
 <script src="JS/jquery.min.js"></script>
-<script src="JS/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="JS/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script src="JS/popper.min.js"></script>
+<script src="JS/bootstrap.min.js"></script>
 <script src="JS/pace.min.js"></script>
 <script src="JS/pageload.js"></script>
