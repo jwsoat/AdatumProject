66020f646ef163ca02706639c5ab29bd95b21772
trogy <33707105+trogy@users.noreply.github.com>
2019-08-20 14:19:25 +1200

    Updated File Paths

--- Backend/Build/indexbuild.php
+++ Backend/Build/indexbuild.php
@@ -1,11 +1,11 @@
 <html>
 <body>
 <?php
-require "/Modules/Head.php"
-require "/Modules/Navbar.php"
-require "/Modules/Pagecontent.php"
-require "/Modules/Footer.php"
-require "/Modules/JS.php"
+require "Modules/Head.php"
+require "Modules/Navbar.php"
+require "Modules/Pagecontent.php"
+require "Modules/Footer.php"
+require "Modules/JS.php"
 ?>
 </body>
 </html>
