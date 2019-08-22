8da0109c20747c934f663e6b8806d4a078e8324b
trogy <33707105+trogy@users.noreply.github.com>
2019-08-22 13:49:09 +1200

    Updated pdf

--- Backend/Build/Pages/backup.php
+++ Backend/Build/Pages/backup.php
@@ -2,12 +2,12 @@
   <object
     data="/Assets/Docs/quote.pdf"
     type="application/pdf"
-    width="100%"
-    height="100%">
+    width="99%"
+    height="90%">
     <iframe
       src="/Assets/Docs/quote.pdf"
-      width="100%"
-      height="100%"
+      width="99%"
+      height="90%"
       style="border: none;">
       <p>Your browser does not support PDFs.
         <a href="https://example.com/test.pdf">Download the PDF</a>.</p>
