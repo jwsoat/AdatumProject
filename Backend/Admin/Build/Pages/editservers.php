1decb8b6c6e296288a9ee3d49166c9318ed624f7
trogy <33707105+trogy@users.noreply.github.com>
2019-08-22 15:59:24 +1200

    Added SQL to admin panel

--- Backend/Admin/Build/Pages/editservers.php
+++ Backend/Admin/Build/Pages/editservers.php
@@ -1,5 +1,12 @@
-<?php require 'Backend/Login/session.php'?>
-<h1> Edit Servers Page </h1>
-<form>
-  <textarea>Edit Text</textarea>
-</form>
+<?php
+require 'Backend/Login/session.php';
+echo "<h1> Edit Servers Page </h1>";
+$sqlquery = "SELECT ServerText FROM Servers";
+$sqlqueryact = mysqli_query($link, $sqlusers) or die(mysqli_error($conn));
+echo "<form>";
+while($showtext = mysqli_fetch_array($sqlqueryact)){
+echo "<textarea>" . $showtext['ServerText'] . "</textarea>";
+}
+echo "</form>";
+mysqli_close();
+?>
