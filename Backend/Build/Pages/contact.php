74f1cc680ce846ece5f18cbe2d7d31af9c95972b
trogy <33707105+trogy@users.noreply.github.com>
2019-08-22 14:58:27 +1200

    Update Contact

--- Backend/Build/Pages/contact.php
+++ Backend/Build/Pages/contact.php
@@ -1,6 +1,6 @@
 <div class="contact-container">
 <h1>Contact Me</h1>
-<h4><a href="mailto:marcanderson4595@gmaail.com">Email Me</a></h4>
+<h4><a href="mailto:admin@adatum.website">Email Me</a></h4>
 <p>Here are my Social Medias
     <a href="https://twitter.com/jwsoat">My twitter</a>
     <a href="https://m.me/jwsoat">Facebook Messenger</a>
