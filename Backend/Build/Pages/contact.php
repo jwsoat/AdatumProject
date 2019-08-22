be9d27426f50079c34159bc179a1ce2b95482acd
trogy <33707105+trogy@users.noreply.github.com>
2019-08-22 15:00:58 +1200

    Updated contact

--- Backend/Build/Pages/contact.php
+++ Backend/Build/Pages/contact.php
@@ -2,7 +2,8 @@
 <h1>Contact Me</h1>
 <h4><a href="mailto:admin@adatum.website">Email Me</a></h4>
 <p>Here are my Social Medias
-    <a href="https://twitter.com/jwsoat">My twitter</a>
+    <a href="https://twitter.com/jwsoat">Jwsoat twitter</a>
+    <a href="https://twitter.com/djtrogy">Trogy twitter</a>
     <a href="https://m.me/jwsoat">Facebook Messenger</a>
 </p>
 </div>
