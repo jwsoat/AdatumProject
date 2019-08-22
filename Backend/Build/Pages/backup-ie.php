f356efe7d52576cd3bb6d54392ec80e02cf718e3
Jwsoat <jwsoat@users.noreply.github.com>
2019-08-22 14:16:41 +1200

    ummm....

---
+++ Backend/Build/Pages/backup-ie.php
@@ -0,0 +1,23 @@
+<div class="backup-container">
+<?php
+include "navbar.html";
+?>
+  <div class="backup-content-container">
+      <a href="/Assets/Docs/quote.zip">45 Drives Backup Server Quote</a>
+      <p>This is old IE</p>
+      <iframe src="/Assets/Docs/quote.pdf" width="100%" height="95%" scrolling="no" ></iframe>
+      
+    <div class="backup-content-left">
+        
+        
+
+    </div>
+    <div class="backup-content-right">
+
+    </div>
+  </div>
+
+</div>
+<?php
+include "footer.html";
+?>
--- Backend/Build/Pages/backup.php
+++ Backend/Build/Pages/backup.php
@@ -1,4 +1,5 @@
 <div class="backup-container">
+<<<<<<< HEAD
   <object
     data="/Assets/Docs/quote.pdf"
     type="application/pdf"
@@ -13,4 +14,13 @@
         <a href="https://example.com/test.pdf">Download the PDF</a>.</p>
     </iframe>
   </object>
+=======
+  <div class="backup-pdf">
+
+  </div>
+  <div class="quote">
+    <iframe src="/Assets/Docs/quote.pdf">
+  </div>
+<a href="/Assets/Docs/quote.pdf">45 Drives Backup Server Quote</a>
+>>>>>>> d37e072b5b7c3c63ae05819af6b49c806e310219
 </div>
