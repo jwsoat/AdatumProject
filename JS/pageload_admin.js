1b7bcb7c9f88230aaab6740fd978bd3e0f3bddcd
trogy <33707105+trogy@users.noreply.github.com>
2019-08-23 23:24:54 +1200

    update admin JS

--- JS/pageload_admin.js
+++ JS/pageload_admin.js
@@ -1,6 +1,11 @@
 //load page content
 $(document).ready(function(){
     $("#adminhome").click(function(){
-        $("#admin-page-content").load("Backend/Build/Pages/home.php");
+        $("#admin-page-content").load("Backend/Admin/Build/Pages/home.php");
+    });
+});
+$(document).ready(function(){
+    $("#adminservers").click(function(){
+        $("#admin-page-content").load("Backend/Admin/Build/Pages/editservers.php");
     });
 });
