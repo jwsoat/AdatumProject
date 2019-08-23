80c7029622e56724c1258bec4496e9408a15ee57
trogy <33707105+trogy@users.noreply.github.com>
2019-08-23 23:58:36 +1200

    Updated Javascript

--- JS/pageload.js
+++ JS/pageload.js
@@ -39,3 +39,20 @@ $(document).ready(function(){
         $("#page-content").load("Backend/Build/Pages/contact.php");
     });
 });
+
+//         _nnnn_
+//        dGGGGMMb     ,"""""""""""""".
+//       @p~qp~~qMb    | Linux Rules! |
+//       M|@||@) M|   _;..............'
+//       @,----.JM| -'
+//      JS^\__/  qKL
+//     dZP        qKRb
+//    dZP          qKKb
+//   fZP            SMMb
+//   HZM            MMMM
+//   FqM            MMMM
+// __| ".        |\dS"qML
+// |    `.       | `' \Zq
+//_)      \.___.,|     .'
+//\____   )MMMMMM|   .'
+//     `-'       `--' hjm
