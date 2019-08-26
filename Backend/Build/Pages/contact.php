d4777c8db49d363722671190771bc9b61fbfd43a
trogy <33707105+trogy@users.noreply.github.com>
2019-08-26 14:53:26 +1200

    Fixed grammar

--- Backend/Build/Pages/contact.php
+++ Backend/Build/Pages/contact.php
@@ -1,10 +1,10 @@
 <div class="contact-container">
-<h1>Contact Me</h1>
+<h1>Contact Us</h1>
 <h4><a href="mailto:admin@adatum.website">Email Me</a></h4>
-<p>Here are my Social Medias
+<p>Here are our Social Medias
     <a href="https://twitter.com/jwsoat">Jwsoat twitter</a>
     <a href="https://twitter.com/djtrogy">Trogy twitter</a>
     <a href="https://m.me/jwsoat">Jwsoat Facebook Messenger</a>
-    <a href="https://m.me/djtrogy">Trogy Facebook Messenger</a>
+    <a href="https://m.me/djtrogy">Trogy Facebook Messenger</a> 
 </p>
 </div>
