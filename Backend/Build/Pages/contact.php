76104defe2dad3a0991c3f4219c46cc26e4be47d
trogy <33707105+trogy@users.noreply.github.com>
2019-08-26 15:01:43 +1200

    Added social buttons

--- Backend/Build/Pages/contact.php
+++ Backend/Build/Pages/contact.php
@@ -2,9 +2,9 @@
 <h1>Contact Us</h1>
 <h4><a href="mailto:admin@adatum.website">Email Me</a></h4>
 <p>Here are our Social Medias
-    <a href="https://twitter.com/jwsoat">Jwsoat twitter</a>
-    <a href="https://twitter.com/djtrogy">Trogy twitter</a>
-    <a href="https://m.me/jwsoat">Jwsoat Facebook Messenger</a>
-    <a href="https://m.me/djtrogy">Trogy Facebook Messenger</a> 
+    <a class="fa fa-twitter" href="https://twitter.com/jwsoat">Jwsoat twitter</a>
+    <a class="fa fa-twitter" href="https://twitter.com/djtrogy">Trogy twitter</a>
+    <a class="fa fa-facebook" href="https://m.me/jwsoat">Jwsoat Facebook Messenger</a>
+    <a class="fa fa-facebook" href="https://m.me/djtrogy">Trogy Facebook Messenger</a> 
 </p>
 </div>
