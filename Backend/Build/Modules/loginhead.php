f6e65d5cf7984a92317d6ae7a9312359b889407d
trogy <33707105+trogy@users.noreply.github.com>
2019-08-26 12:57:19 +1200

    removed admin stuff

--- Backend/Build/Modules/loginhead.php
+++
@@ -1,7 +0,0 @@
-<head>
-  <title>Login</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../../CSS/bootstrap.min.css">
-    <link rel="stylesheet" href="../../CSS/cssadmin.css">
-</head>
