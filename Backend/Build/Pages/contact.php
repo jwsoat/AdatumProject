5f4364b9d6d07d8a33ac3e587f2d7f7bb9469d8f
trogy <33707105+trogy@users.noreply.github.com>
2019-08-26 15:36:05 +1200

    updated thing

--- Backend/Build/Pages/contact.php
+++ Backend/Build/Pages/contact.php
@@ -1,16 +1,25 @@
 <div class="contact-container">
 <h1>Contact Us</h1>
 <h4><a href="mailto:admin@adatum.website">Email Me</a></h4>
-<p>Here are our Social Medias
+<h2>Social Medias</h2>
     <a class="fa fa-twitter" href="https://twitter.com/jwsoat">Jwsoat twitter</a>
     <a class="fa fa-twitter" href="https://twitter.com/djtrogy">Trogy twitter</a>
     <a class="fa fa-facebook" href="https://m.me/jwsoat">Jwsoat Facebook Messenger</a>
     <a class="fa fa-facebook" href="https://m.me/djtrogy">Trogy Facebook Messenger</a>
+<h3> Trogy's Social </h3>
     <a class="btn btn-social-icon btn-twitter">
       <span class="fa fa-twitter"></span>
     </a>
     <a class="btn btn-social-icon btn-facebook">
       <span class="fa fa-facebook"></span>
     </a>
-</p>
+<h3> Jwsoat's Social </h3>
+  <a class="btn btn-social-icon btn-twitter">
+    <span class="fa fa-twitter"></span>
+  </a>
+  <a class="btn btn-social-icon btn-facebook">
+    <span class="fa fa-facebook"></span>
+  </a>
+
+
 </div>
