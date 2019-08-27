9250d16075f49d20a60155d71e57fb7154e61501
trogy <33707105+trogy@users.noreply.github.com>
2019-08-27 13:51:30 +1200

    Update social buttons

--- Backend/Build/Pages/contact.php
+++ Backend/Build/Pages/contact.php
@@ -3,19 +3,10 @@
 <h4><a href="mailto:admin@adatum.website">Email Me</a></h4>
 <h2>Social Medias</h2>
 <h3> Trogy's Social </h3>
-    <a class="btn btn-social-icon btn-twitter" href="https://twitter.com/djtrogy">
-      <span class="fab fa-twitter"></span>
-    </a>
-    <a class="btn btn-social-icon btn-facebook" href="https://m.me/djtrogy">
-      <span class="fab fa-facebook"></span>
-    </a>
+<button type="button" class="btn" style="background-color:#1DA1F2; color:white;" href="twitter.com/djtrogy"><i class="fab fa-twitter pr-1"></i> Twitter</button>
+<button type="button" class="btn" style="background-color:#3b5998; color:white;" href="m.me/djtrogy"><i class="fab fa-facebook-f pr-1"></i> Facebook</button>
 <h3> Jwsoat's Social </h3>
-  <a class="btn btn-social-icon btn-twitter" href="https://twitter.com/jwsoat">
-    <span class="fab fa-twitter"></span>
-  </a>
-  <a class="btn btn-social-icon btn-facebook" href="https://m.me/jwsoat">
-    <span class="fab fa-facebook"></span>
-  </a>
-
+<button type="button" class="btn" style="background-color:#1DA1F2; color:white;" href="twitter.com/jwsoat"><i class="fab fa-twitter pr-1"></i> Twitter</button>
+<button type="button" class="btn" style="background-color:#3b5998; color:white;" href="m.me/jwsoat"><i class="fab fa-facebook-f pr-1"></i> Facebook</button>
 
 </div>
