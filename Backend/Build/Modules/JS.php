be9bdec8e4ba6b3c51d662807e783f99d1bb011d
trogy <33707105+trogy@users.noreply.github.com>
2019-08-27 13:01:50 +1200

    Added webfonts

--- Backend/Build/Modules/JS.php
+++ Backend/Build/Modules/JS.php
@@ -4,4 +4,3 @@
 <script src="JS/bootstrap.min.js"></script>
 <script src="JS/pageload.js"></script>
 <script src="JS/all.js"></script>
-<script src="https://www.powr.io/powr.js?platform=html"></script>
