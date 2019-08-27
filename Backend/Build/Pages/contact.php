8ca8402d3e206641709722a17f576c7a2484f83a
trogy <33707105+trogy@users.noreply.github.com>
2019-08-27 13:56:39 +1200

    Updated contact

--- Backend/Build/Pages/contact.php
+++ Backend/Build/Pages/contact.php
@@ -3,9 +3,11 @@
 <h4><a href="mailto:admin@adatum.website">Email Me</a></h4>
 <h2>Social Medias</h2>
 <h3> Trogy's Social </h3>
-<button type="button" class="btn" style="background-color:#1DA1F2; color:white;" href="twitter.com/djtrogy"><i class="fab fa-twitter pr-1"></i> Twitter</button>
-<button type="button" class="btn" style="background-color:#3b5998; color:white;" href="m.me/djtrogy"><i class="fab fa-facebook-messenger"></i> Facebook Messenger</button>
+<button type="button" class="btn" style="background-color:#1DA1F2; color:white;" href="twitter.com/djtrogy"><i class="fab fa-twitter"></i> Twitter</button>
+<button type="button" class="btn" style="background-color:#3b5998; color:white;" href="m.me/djtrogy"><i class="fab fa-facebook-messenger"></i> Messenger</button>
+<button type="button" class="btn" style="background-color:#211F1F; color:white;" href="github.com/trogy"><i class="fab fa-github"></i> Github</button>
 <h3> Jwsoat's Social </h3>
-<button type="button" class="btn" style="background-color:#1DA1F2; color:white;" href="twitter.com/jwsoat"><i class="fab fa-twitter pr-1"></i> Twitter</button>
-<button type="button" class="btn" style="background-color:#3b5998; color:white;" href="m.me/jwsoat"><i class="fab fa-facebook-messenger"></i> Facebook Messenger</button>
+<button type="button" class="btn" style="background-color:#1DA1F2; color:white;" href="twitter.com/jwsoat"><i class="fab fa-twitter"></i> Twitter</button>
+<button type="button" class="btn" style="background-color:#3b5998; color:white;" href="m.me/jwsoat"><i class="fab fa-facebook-messenger"></i> Messenger</button>
+<button type="button" class="btn" style="background-color:#211F1F; color:white;" href="github.com/jwsoat"><i class="fab fa-github"></i> Github</button>
 </div>
