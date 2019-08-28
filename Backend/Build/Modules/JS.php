c58fe30bef5b5bfc971a8291fbd5005520df5f64
trogy <33707105+trogy@users.noreply.github.com>
2019-08-28 16:29:54 +1200

    Added D3

--- Backend/Build/Modules/JS.php
+++ Backend/Build/Modules/JS.php
@@ -3,5 +3,6 @@
 <script src="JS/popper.min.js"></script>
 <script src="JS/bootstrap.min.js"></script>
 <script src="JS/billboard.min.js"></script>
+<script src="JS/d3.v5.min.js"></script>
 <script src="JS/pageload.js"></script>
 <script src="JS/all.js"></script>
