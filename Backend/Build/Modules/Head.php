01d1b6fd842c46991a94cbfec14069c8011ce581
trogy <33707105+trogy@users.noreply.github.com>
2019-08-28 16:24:52 +1200

    Added Billboard Pending Future Graphs

--- Backend/Build/Modules/Head.php
+++ Backend/Build/Modules/Head.php
@@ -4,5 +4,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="CSS/bootstrap.min.css">
     <link rel="stylesheet" href="CSS/all.css">
+    <link rel="stylesheet" href="CSS/insight.min.css">
     <link rel="stylesheet" href="CSS/cssmain.css">
 </head>
--- Backend/Build/Modules/JS.php
+++ Backend/Build/Modules/JS.php
@@ -2,5 +2,6 @@
 <script src="JS/jquery.min.js"></script>
 <script src="JS/popper.min.js"></script>
 <script src="JS/bootstrap.min.js"></script>
+<script src="JS/billboard.min.js"></script>
 <script src="JS/pageload.js"></script>
 <script src="JS/all.js"></script>
