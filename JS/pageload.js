3179eeeb06628faf5a7ffdd129ea205c16ae7eef
trogy <33707105+trogy@users.noreply.github.com>
2019-08-28 16:05:46 +1200

    Added logs

--- JS/pageload.js
+++ JS/pageload.js
@@ -44,43 +44,52 @@ $(document).ready(function(){
 function trogy_twitter()
 {
     window.open ("https://twitter.com/djtrogy");
-    window.href ("/EE/EE.html")
+    console.log("Loaded Trogy's Twitter!");
 }
 function jwsoat_twitter()
 {
     window.open ("https://twitter.com/jwsoat");
+    console.log("Loaded Jwsoat's Twitter!");
 }
 function trogy_messenger()
 {
     window.open ("https://m.me/djtrogy");
+    console.log("Loaded Trogy's Messenger!");
 }
 function jwsoat_messenger()
 {
     window.open ("https://m.me/jwsoat");
+    console.log("Loaded Jwsoat's Messenger!");
 }
 function trogy_github()
 {
     window.open ("https://github.com/trogy");
+    console.log("Loaded Trogy's Github!");
 }
 function jwsoat_github()
 {
     window.open ("https://github.com/jwsoat");
+    console.log("Loaded Jwsoat's Github!");
 }
+function yourfoundme()
+{
+      window.href ("/EE/EE.html")
+      console.log("You Found The Easter Egg!");
+      console.log("         _nnnn_");
+      console.log("        dGGGGMMb     ,"""""""""""""".");
+      console.log("       @p~qp~~qMb    | Linux Rules! |");
+      console.log("       M|@||@) M|   _;..............'");
+      console.log("       @,----.JM| -'");
+      console.log("      JS^\__/  qKL");
+      console.log("     dZP        qKRb");
+      console.log("    dZP          qKKb");
+      console.log("   fZP            SMMb");
+      console.log("   HZM            MMMM");
+      console.log("   FqM            MMMM");
+      console.log(" __| ".        |\dS"qML");
+      console.log(" |    `.       | `' \Zq");
+      console.log("_)      \.___.,|     .'");
+      console.log("\____   )MMMMMM|   .'");
+      console.log("     `-'       `--' hjm");
 
-
-//         _nnnn_
-//        dGGGGMMb     ,"""""""""""""".
-//       @p~qp~~qMb    | Linux Rules! |
-//       M|@||@) M|   _;..............'
-//       @,----.JM| -'
-//      JS^\__/  qKL
-//     dZP        qKRb
-//    dZP          qKKb
-//   fZP            SMMb
-//   HZM            MMMM
-//   FqM            MMMM
-// __| ".        |\dS"qML
-// |    `.       | `' \Zq
-//_)      \.___.,|     .'
-//\____   )MMMMMM|   .'
-//     `-'       `--' hjm
+}
