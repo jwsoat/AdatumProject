9b706d1f68e3b89ac78bfae29d17109653b306a1
trogy <33707105+trogy@users.noreply.github.com>
2019-08-29 14:21:05 +1200

    added security

--- Backend/Build/Pages/security.php
+++ Backend/Build/Pages/security.php
@@ -1,9 +1,34 @@
 <!-- Place Everything Inside Main DIV -->
 <div class="security-container animated fadeIn delay-0.5s">
-  <h1>Security</h1>
+  <h1>Privacy and Security</h1>
   <div class="security-content-container">
     <div class="security-content-left">
-  <p>
+<h3>Privacy<h3>
+
+<h4>Disk Destruction<br><br>
+Kill Disk</h4>
+ <ul>
+<li>Kill Disk is an affordable industrial-grade disk sanitation hardware solution combined with Industrial Software and supporting parallel erasure of up to 25 HDD/SSD disks.</li>
+<li>Rack mountable solutions, 3U & 4U form factors.</li>
+<li>Customizable, quiet & energy efficient</li>
+<li>Supports fully automated operations.</li>
+<li>Licensing policy assumes no extra costs per disk erase!</li>
+</ul>
+<h4>Security Cameras<br><br>
+UniFi Video IP Camera G4 Pro</h4>
+<ul>
+<li>The UVC-G4-PRO is easy to set up, operate, and manage using the UniFi Protect software</li>
+<li>Integrated High-Power Infrared LEDs for Night Vision</li>
+<li>Built-in Microphone</li>
+<li>4K Ultra HD (3840 x 2160), 24 FPS</li>
+<li>Comes pre-installed the UCK-G2-PLUS, the latest generation Cloud Key controller.</li>
+</ul>
+<h4>Security<h4>
+<ul>
+<li>RFID Cards (for entrance to building)</li>
+<li>Security alarm system (for the building and server room)</li>
+<li>Receptionist (for the building)</li>
+</ul>
   </p>
     </div>
     <div class="security-content-right animated slideInRight delay-1s">
