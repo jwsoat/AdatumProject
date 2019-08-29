43f209f3e4dce295b1b2085aff6c117f2f68ad50
Jwsoat <jwsoat@users.noreply.github.com>
2019-08-29 14:33:31 +1200

    fixed black text

--- Backend/Build/Pages/software.php
+++ Backend/Build/Pages/software.php
@@ -5,16 +5,14 @@
     <div class="software-content-left">
     <h4>Windows Server 2016 </h4>
       <h4>DHCP Server.</h4>
-      The DHCP Server gives client computers a lease ip so they can connect to the internet .
+      <p>The DHCP Server gives client computers a lease ip so they can connect to the internet</p>
 
       <h4>DNS Server.</h4>
-      The Active Directory creates authentication, updating, or searching, computers by using DNS Server to locate Domain controller. The domain controller use a DNS server to trace other computers.
+      <p>The Active Directory creates authentication, updating, or searching, computers by using DNS Server to locate Domain controller. The domain controller use a DNS server to trace other computers.</p>
 
       <h4>Active Directory Domain Services</h4>
-      A server running Active Directory Domain Services is called a domain controller. It authenticates all users and computers in a Windows domain type network assigning and enforcing security policies for all computers and installing or updating software. Active Directory checks the submitted password and searches the database if the user is an administrator account or normal user account. Also, it allows management and storage of information, provides authentication and authorization mechanisms.
+      <p>A server running Active Directory Domain Services is called a domain controller. It authenticates all users and computers in a Windows domain type network assigning and enforcing security policies for all computers and installing or updating software. Active Directory checks the submitted password and searches the database if the user is an administrator account or normal user account. Also, it allows management and storage of information, provides authentication and authorization mechanisms.</p>
       <a href="https://en.wikipedia.org/wiki/Active_Directory">Source: Wikipedia</a>
-
-  </p>
     </div>
     <div class="software-content-right animated slideInRight delay-1s">
 <img style="border-radius: 2%;" src="" alt="ADD AN IMAGE HERE" height="auto" width="100%">
