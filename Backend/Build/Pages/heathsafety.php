32c6bd3e54aba77c97e8348c06d553b441847f0c
Jwsoat <jwsoat@users.noreply.github.com>
2019-08-29 14:12:19 +1200

    healthandsafrty text added

--- Backend/Build/Pages/heathsafety.php
+++ Backend/Build/Pages/heathsafety.php
@@ -3,8 +3,36 @@
   <h1>Heath & Safety</h1>
   <div class="heathsafety-content-container">
     <div class="heathsafety-content-left">
-  <p>
-  </p>
+      <h4>Server Room (Hazards)</h4>
+      <p>
+        Tripping hazards
+        Electricity
+        Fans
+        Eating food
+        Drinking water
+        Temperature
+      </p>
+      <h4>Office (Hazards)</h4>
+      <p>
+          Chairs and tables
+          Tripping hazards
+          Not blocking the exits
+          Sickness\injuries
+          Children
+          Poor posture
+          Eye strain
+          How are we going to achieve Health and Safety in the Office or Server Room
+          Evaluation plan\procedures
+          Having fire extinguisher
+          Having First aid kit
+          Having exit signs placed
+          Having a fire alarm system in place
+          Assign a person to be a fire warden
+          Having an Evaluation Meeting Point planned
+          Don’t use evaluators use the stairs
+          Go to the closest emergency exit
+          Have a meeting to discuss about health and safety
+        </p>
     </div>
     <div class="heathsafety-content-right animated slideInRight delay-1s">
 <img style="border-radius: 2%;" src="" alt="ADD AN IMAGE HERE" height="auto" width="100%">
