920ac723ffb5952961d6b1842bd217fbfe17db7c
Jwsoat <jwsoat@users.noreply.github.com>
2019-08-29 14:27:14 +1200

    Update software.php

--- Backend/Build/Pages/software.php
+++ Backend/Build/Pages/software.php
@@ -3,7 +3,17 @@
   <h1>Software</h1>
   <div class="software-content-container">
     <div class="software-content-left">
-  <p>
+    <h4>Windows Server 2016 </h4>
+      <h4>DHCP Server.</h4>
+      The DHCP Server gives client computers a lease ip so they can connect to the internet .
+
+      <h4>DNS Server.</h4>
+      The Active Directory creates authentication, updating, or searching, computers by using DNS Server to locate Domain controller. The domain controller use a DNS server to trace other computers.
+
+      <h4>Active Directory Domain Services</h4>
+      A server running Active Directory Domain Services is called a domain controller. It authenticates all users and computers in a Windows domain type network assigning and enforcing security policies for all computers and installing or updating software. Active Directory checks the submitted password and searches the database if the user is an administrator account or normal user account. Also, it allows management and storage of information, provides authentication and authorization mechanisms.
+      Source: Wikipedia
+
   </p>
     </div>
     <div class="software-content-right animated slideInRight delay-1s">
