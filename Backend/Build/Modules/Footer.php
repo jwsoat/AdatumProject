cf2fbc8049cda6a5763f96ef27ded97a31f88db4
trogy <33707105+trogy@users.noreply.github.com>
2019-10-01 11:11:03 +1300

    Changed Footer

--- Backend/Build/Modules/Footer.php
+++ Backend/Build/Modules/Footer.php
@@ -1,3 +1,3 @@
 <div class="footer">
-<p>  < > By Jwsoat, Trogy, Jfepuleai11 & eiberg10 (C) <?php echo date("Y");?> App loaded at <?php echo date("D M j G:i:s T Y"); ?> </p>
+<p>  < > By Jwsoat, Trogy, Jfepuleai11 & eiberg10 (C) <?php echo date("Y");?> | App loaded at <?php echo date("D M j G:i:s T Y"); ?> </p>
 </div>
