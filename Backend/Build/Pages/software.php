eea654df61c082b80fb3abe90c7da1db5b48e5aa
trogy <33707105+trogy@users.noreply.github.com>
2019-10-01 14:05:41 +1300

    Updated Software Page

--- Backend/Build/Pages/software.php
+++ Backend/Build/Pages/software.php
@@ -4,14 +4,14 @@
   <div class="software-content-container">
     <div class="software-content-left">
       <h4>Windows Server 2016 </h4>
-      <h4>DHCP Server.</h4>
+      <h4>Dynamic Host Configuration Protocol (DHCP) Server</h4>
       <p>The DHCP Server gives client computers a lease ip so they can connect to the internet</p>
-      <h4>DNS Server.</h4>
+      <h4>Domain Name Server (DNS)</h4>
       <p>The Active Directory creates authentication, updating, or searching, computers by using DNS Server to locate Domain controller. The domain controller use a DNS server to trace other computers.</p>
-      <h4>Active Directory Domain Services</h4>
+      <h4>Active Directory Domain Services (ADDS)</h4>
       <p>A server running Active Directory Domain Services is called a domain controller. It authenticates all users and computers in a Windows domain type network assigning and enforcing security policies for all computers and installing or updating software. Active Directory checks the submitted password and searches the database if the user is an administrator account or normal user account. Also, it allows management and storage of information, provides authentication and authorization mechanisms.</p>
       <a href="https://en.wikipedia.org/wiki/Active_Directory">Source: Wikipedia</a>
-      <h4>VPN</h4>
+      <h4>Virtual Private Networks (VPN)</h4>
       <p>The Remote Access Service is used as a single tenant gateway for the VPN server that could provide many advance features and functionality. Users can access Local data servers by connecting to locally hosted VPN server. This makes it so users can access the data offsite.</p>
       <h4> Windows 10 </h4>
       <p>Windows 10 is a great operating system for everyone doesn't matter if your a beginner or not is friendly UI makes it useful for everyone</p>
